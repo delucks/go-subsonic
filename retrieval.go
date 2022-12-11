ad5da13a51e383f36a085c289ca5d738773b03ec
Drew Weymouth <dweymouth@gmail.com>
2022-12-11 11:11:22 -0800

    add GetStreamURL

--- retrieval.go
+++ retrieval.go
@@ -55,6 +55,30 @@ func (s *Client) Stream(id string, parameters map[string]string) (io.Reader, err
 	return response.Body, nil
 }
 
+// GetStreamURL returns the URL for streaming the specified media. Similar to Stream,
+// except it does not actually perform the HTTP request. Useful when the streaming
+// will be handled by an outside program, e.g. mpv.
+//
+// Optional Parameters:
+//   maxBitRate:             (Since 1.2.0) If specified, the server will attempt to limit the bitrate to this value, in kilobits per second. If set to zero, no limit is imposed.
+//   format:                 (Since 1.6.0) Specifies the preferred target format (e.g., "mp3" or "flv") in case there are multiple applicable transcodings.  Starting with 1.9.0 you can use the special value "raw" to disable transcoding.
+//   timeOffset:             Only applicable to video streaming. If specified, start streaming at the given offset (in seconds) into the video. Typically used to implement video skipping.
+//   size:                   (Since 1.6.0) Only applicable to video streaming. Requested video size specified as WxH, for instance "640x480".
+//   estimateContentLength:  (Since 1.8.0). If set to "true", the Content-Length HTTP header will be set to an estimated value for transcoded or downsampled media.
+//   converted:              (Since 1.14.0) Only applicable to video streaming. Subsonic can optimize videos for streaming by converting them to MP4. If a conversion exists for the video in question, then setting this parameter to "true" will cause the converted video to be returned instead of the original.
+func (s *Client) GetStreamURL(id string, parameters map[string]string) (*url.URL, error) {
+	params := url.Values{}
+	params.Add("id", id)
+	for k, v := range parameters {
+		params.Add(k, v)
+	}
+	req, err := s.setupRequest("GET", "stream", params)
+	if err != nil {
+		return nil, err
+	}
+	return req.URL, nil
+}
+
 // Download returns a given media file. Similar to stream, but this method returns the original media data without transcoding or downsampling.
 func (s *Client) Download(id string) (io.Reader, error) {
 	params := url.Values{}
